d9fdb4b5bc360f11fc20e2aacdd83cfb5eb499c3
Nicholas Bentzen <bentzen.nich@gmail.com>
2020-01-04 21:59:40 -0600

    Initial commit

---
+++ FizzBuzz.py
@@ -0,0 +1,20 @@
+def main():
+    fizz_buzz()
+
+
+def fizz_buzz():
+    word = ""
+    for i in range(1, 101):
+        word = i
+        if check_number(i, 15):
+            word = "FizzBuzz"
+        elif check_number(i, 5):
+            word = "Buzz"
+        elif check_number(i, 3):
+            word = "Fizz"
+        print(word)
+    return
+
+
+def check_number(current_number, mod_number):
+    return current_number % mod_number == 0
